98cf2f815f0b73ef6f291abd6df4cfce7decdc34
furrki <furkankaynary@gmail.com>
2018-09-30 00:31:44 +0300

    Economy System

--- routes/economy.js
+++ routes/economy.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res) {
     res.render("Template",{
-        page:"Economy",
+        page:"AllCurrencies",
         args: {
-            
+
+        }
+    })
+});
+router.get('/currency', function(req, res) {
+    res.render("Template",{
+        page:"AllCurrencies",
+        args: {
+
+        }
+    })
+});
+router.get('/currency/:name', function(req, res) {
+    res.render("Template",{
+        page:"Currency",
+        args: {
+            name: req.params.name
         }
     })
 });
