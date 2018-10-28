e27240fd911e51eefa960f7f35cabae2f1a75a75
furrki <furkankaynary@gmail.com>
2018-10-28 12:21:09 -0700

    Login System

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ var api = new ParseServer({
   masterKey:  'MASTER_KEY', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-      classNames: []
+      classNames: ["Room", "Message"]
     }
 });
 
